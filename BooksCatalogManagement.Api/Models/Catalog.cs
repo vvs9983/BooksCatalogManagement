01e514ae25e89bcf1f71c6ee7ac46088337254a6
Victor Sinaniev <victor.sinaniev@quality-line.org>
2017-03-11 18:22:15 +0200

    Catalog class added

---
+++ BooksCatalogManagement.Api/Models/Catalog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+
+namespace BooksCatalogManagement.Api.Models
+{
+    [XmlRoot("catalog")]
+    public class Catalog
+    {
+        [XmlArray("book")]
+        public List<Book> BooksCatalog { get; set; }
+    }
+}
