004e70b46c348ef798a4aea054b054269692a614
Victor Sinaniev <victor.sinaniev@quality-line.org>
2017-03-11 19:55:25 +0200

    1. Main window added 2. Logic class added

---
+++ BooksCatalogManagement.Gui/Main.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace BooksCatalogManagement.Gui
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewCatalog = new System.Windows.Forms.DataGridView();
+            this.buttonSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCatalog)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewCatalog
+            // 
+            this.dataGridViewCatalog.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewCatalog.Location = new System.Drawing.Point(12, 12);
+            this.dataGridViewCatalog.Name = "dataGridViewCatalog";
+            this.dataGridViewCatalog.Size = new System.Drawing.Size(358, 199);
+            this.dataGridViewCatalog.TabIndex = 0;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Enabled = false;
+            this.buttonSave.Location = new System.Drawing.Point(12, 226);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 1;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 261);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.dataGridViewCatalog);
+            this.Name = "Main";
+            this.Text = "Main";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCatalog)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewCatalog;
+        private System.Windows.Forms.Button buttonSave;
+    }
+}
--- BooksCatalogManagement.Gui/Main.cs
+++ BooksCatalogManagement.Gui/Main.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BooksCatalogManagement.Api.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,9 +11,11 @@ using System.Windows.Forms;
 
 namespace BooksCatalogManagement.Gui
 {
-    public partial class Form1 : Form
+    public partial class Main : Form
     {
-        public Form1()
+        private Catalog _catalog;
+
+        public Main()
         {
             InitializeComponent();
         }
