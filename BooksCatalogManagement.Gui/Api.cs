f3c4bc65f0cbb723596a503986e6ab0d3fbb94b3
Victor Sinaniev <victor.sinaniev@quality-line.org>
2017-03-11 20:17:13 +0200

    Api class added

---
+++ BooksCatalogManagement.Gui/Api.cs
@@ -0,0 +1,56 @@
+﻿using BooksCatalogManagement.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BooksCatalogManagement.Gui
+{
+    class Api
+    {
+        private HttpClient httpClient;
+
+        public Api()
+        {
+            httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(@"http://localhost:51556/");
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+        }
+
+        public async Task<Catalog> GetCatalog()
+        {
+            string catalogContent = string.Empty;
+
+            try
+            {
+                HttpResponseMessage httpResponseMessage = await httpClient.GetAsync("api/catalog");
+
+                if (httpResponseMessage.IsSuccessStatusCode)
+                {
+                    catalogContent = await httpResponseMessage.Content.ReadAsStringAsync();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return Deserialize(catalogContent);
+        }
+
+        private Catalog Deserialize(string xml)
+        {
+            using (var stringReader = new StringReader(xml))
+            {
+                var xmlSerializer = new XmlSerializer(typeof(Catalog));
+
+                return xmlSerializer.Deserialize(stringReader) as Catalog;
+            }
+        }
+    }
+}
